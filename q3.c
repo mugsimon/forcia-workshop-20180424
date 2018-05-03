f128703a8a0c1353d3ba9d355ab0392bb8c7a5ff
mugsimon <mug24375826dev@gmail.com>
2018-05-03 23:46:49 +0900

    fix print_answer with debug code

--- q3.c
+++ q3.c
@@ -38,13 +38,15 @@ void quick_sort(int array[], int n){
 
 void init_array(int array[], int n){
   int i;
-  for(i = 0; i < n; i++){
+  for(i = 0; i <= n; i++){
     array[i] = 0;
   }
 }
 
-void optimize_array(int array[], int n, int newarray[], int min, int max){
-  int i, j;
+void optimize_array(int array[], int n, int newarray[]){
+  int i, j, min, max;
+  min = array[0];
+  max = array[n-1];
   init_array(newarray, max);
   for(i = 0, j = min; i < n; i++){ 
     while(array[i] > j){
@@ -56,13 +58,24 @@ void optimize_array(int array[], int n, int newarray[], int min, int max){
 }
 
 int search(int array[], int min, int max){
+  printf("min=%d, max=%d\n", min, max);//
+  printf("array[%d]=%d, array[%d]=%d, array[%d]=%d\n", min-1, array[min-1], min, array[min], max, array[max]);
   return array[max]-array[min-1];
 }
 
-void print_answer(int oarray[], int qarray[], int q){
-  int i;
+void print_answer(int oarray[], int min, int max, int qarray[], int q){
+  int i, answer;
+  printf("min=%d, max=%d\n", min, max);//
   for(i = 0; i < q * 2; i += 2){
-    printf("%d\n", search(oarray, qarray[i], qarray[i+1]));
+    if(qarray[i]>max){
+      answer=0;
+    }else if(qarray[i+1]>max){
+      answer=search(oarray, qarray[i], max);
+    }else{
+      answer=search(oarray, qarray[i], qarray[i+1]);
+    }
+    printf("(%d, %d)=", qarray[i], qarray[i+1]);//
+    printf("%d\n", answer);
   }
 }
   
@@ -72,7 +85,15 @@ void get_array(int array[], int n){
     array[i] = read_int();
   }
 }
-
+//
+void print_array(int array[], int n){
+  int i;
+  for(i=0;i<n;i++){
+    printf("array[%d]=%d ", i, array[i]);
+  }
+  printf("\n");
+}
+//
 void forcia_workshop(void){
   int n, q;
   n = read_int();
@@ -82,12 +103,15 @@ void forcia_workshop(void){
   get_array(inputArray, n);
   get_array(queryArray, q * 2);
   quick_sort(inputArray, n);
-  int min, max;
+  int min, max, newN;
   min = inputArray[0];
   max = inputArray[n-1];
-  int optimizedArray[max+1];
-  optimize_array(inputArray, n, optimizedArray, min, max+1);
-  print_answer(optimizedArray, queryArray, q);
+  newN = max + 1;
+  int optimizedArray[newN];
+  optimize_array(inputArray, n, optimizedArray);
+  print_array(optimizedArray, newN);//
+  
+  print_answer(optimizedArray, min, max, queryArray, q);
 }
 
 int main(){
