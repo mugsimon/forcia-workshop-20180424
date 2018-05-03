80df2eef9897a1eed09fd3602a6f9f79f3483d69
mugsimon <mug24375826dev@gmail.com>
2018-05-03 18:31:10 +0900

    delete debug code

--- q3.c
+++ q3.c
@@ -17,12 +17,6 @@ void swap(int array[], int i, int j){
 }
 
 int main(){
-  int a[10]={3,4,8,9,1,2,5,6,0,7};
-  printf("%d\n", sizeof(a));
-  
-  print_array(a);
-  quick_sort(a);
-  print_array(a);
   return 0;
 }
 
